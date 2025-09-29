b9f077f7ca30211b39bde26d1e33fb314a5e57d4
Gui-diegues <guilhermediegues2006@gmail.com>
2025-09-29 11:56:37 -0300

    Adicionado funcionários + setlocale

--- loja_de_calcados.cpp
+++ loja_de_calcados.cpp
@@ -2,9 +2,92 @@
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
+#include <locale.h>
 
+#define LISTA_FUNCIONARIO 100000
 #define MAX_PRODUTOS 100 
 #define MAX_VENDAS 100 
+
+
+//função dos dados dos funcionarios
+typedef struct{
+	int matricula;
+	char nome [50];
+	char endereco [30];
+	int telefone;
+	int dia;
+	int mes;
+	int ano;
+	int salario;
+	char experiencia [200];
+	
+} funcionario;
+
+funcionario lista [LISTA_FUNCIONARIO];
+int listas ;
+
+void cadastrarfuncionario() {
+	funcionario f;
+	
+	
+	printf("Matrícula: ");
+	scanf("%d", &f.matricula);
+	getchar();
+	
+	printf("Nome: ");
+	fgets(f.nome, 50, stdin);
+	f.nome[strcspn(f.nome, "\n")] = 0;
+	
+	printf("Endereço: ");
+	fgets(f.endereco, 30, stdin);
+	f.endereco[strcspn(f.endereco, "\n")] = 0;
+	
+	printf("Telefone: ");
+	scanf("%d", &f.telefone);
+	
+	printf("Data de nascimento:\n");
+	
+	printf("Dia: ");
+	scanf("%d", &f.dia);
+	
+	printf("Mês: ");
+	scanf("%d", &f.mes);
+	
+	printf("Ano: ");
+	scanf("%d", &f.ano);
+	
+	printf("Salário: ");
+	scanf("%d", &f.salario);
+	getchar();
+	
+	printf("Experiência: ");
+	fgets(f.experiencia, 200, stdin);
+	f.experiencia[strcspn(f.experiencia, "\n")] = 0;
+	
+	lista[listas] = f;
+	listas++;
+	printf("Funcionário cadastrado com sucesso!/n");
+}
+
+// função de vizualizar funcionários
+void visualizarFuncionarios () {
+	if (listas == 0) {
+		printf("\nNenhum funcionário cadastrado ainda.\n");
+	} else {
+		printf("\n--- Lista de Funcionários ---\n");
+		for (int i = 0; i < listas; i++) {
+			printf("\nFuncionário %d: \n", i + 1);
+			printf("Matrícula: %d\n", lista[i].matricula);
+			printf("Nome: %s\n", lista[i].nome);
+			printf("Endereço: %s\n", lista[i].endereco);
+			printf("Telefone: %d\n", lista[i].telefone);
+			printf("Data de nascimento: %02d/%02d/%04d\n", lista[i].dia, lista[i].mes, lista[i].ano);
+			printf("Salário: R$ %d\n", lista[i].salario);
+			printf("Experiência: %s\n", lista[i].experiencia);
+		}
+	}
+}
+
 
 //representacao dos produtos
 typedef struct {
@@ -88,7 +171,7 @@ void listarProdutos() {
     }
 
     for (int i = 0; i < total; i++) {
-        printf("Codigo: %d | Descricao: %s | Tamanho: %d | Cor: %s | Marca: %s | Quantidade: %d | Valor: %.2f\n",
+        printf("Código: %d | Descricao: %s | Tamanho: %d | Cor: %s | Marca: %s | Quantidade: %d | Valor: %.2f\n",
                estoque[i].codigo,
                estoque[i].descricao,
                estoque[i].tamanho,
@@ -102,7 +185,7 @@ void listarProdutos() {
 // buscar os produto pelo codigo
 void buscarProduto() {
     int codigo;
-    printf("Digite o codigo do produto: ");
+    printf("Digite o código do produto: ");
     scanf("%d", &codigo);
 
     for (int i = 0; i < total; i++) {
@@ -118,13 +201,13 @@ void buscarProduto() {
             return;
         }
     }
-    printf("Produto nao encontrado.\n");
+    printf("Produto não encontrado.\n");
 }
 
 // Efetuar venda
 void efetuarVenda() {
     if (total == 0) {
-        printf("Nao ha produtos cadastrados!\n");
+        printf("Não há produtos cadastrados!\n");
         return;
     }
 
@@ -164,18 +247,19 @@ void efetuarVenda() {
             printf("Produto: %s\n", estoque[i].descricao);
             printf("Marca: %s\n", estoque[i].marca);
             printf("Quantidade: %d\n", qtd);
-            printf("Valor unitario: %.2f\n", estoque[i].valor);
+            printf("Valor unitário: %.2f\n", estoque[i].valor);
             printf("Total: %.2f\n", totalVenda);
             printf("=========================\n");
 
             return;
         }
     }
-    printf("Produto nao encontrado.\n");
+    printf("Produto não encontrado.\n");
 }
 
 int main(){
     int opcao, opcaoitem;
+    setlocale(LC_ALL, "Portuguese");
     
     do{ 
     printf("LOJA DE CALCADOS\n\n");
@@ -185,7 +269,23 @@ int main(){
         
     switch(opcao){
          case 1 :
-        	printf("Funcionalidade de funcionario ainda nÃ£o implementada \n");
+        	do {
+        printf("\n--- MENU FUNCIONÁRIOS ---\n");
+        printf("1 - Cadastrar funcionário\n");
+        printf("2 - Visualizar funcionários\n");
+        printf("3 - Sair\n");
+        printf("Escolha: ");
+        scanf("%d", &opcao);
+        getchar();
+
+        switch (opcao) {
+            case 1: cadastrarfuncionario(); break;
+            case 2: visualizarFuncionarios(); break;
+            case 3: printf("Saindo do programa...\n"); break;
+            default: printf("Opção inválida! Tente novamente.\n");
+        }
+
+    } while (opcao != 3);
             break;
         case 2 :
             do { 
@@ -202,7 +302,7 @@ int main(){
             case 2: listarProdutos(); break;
             case 3: buscarProduto(); break;
             case 0: printf("Saindo...\n"); break;
-            default: printf("Opcao invalida!\n");
+            default: printf("Opcão inválida!\n");
         }
     } while (opcaoitem != 0);
     break;
@@ -213,7 +313,7 @@ int main(){
 		printf("Saindo do programa...\n");
 		break;
     default :
-    	printf("OpÃ§Ã£o invÃ¡lida\n");
+    	printf("Opcão inválida!\n");
     }
     
 } while(opcao != 4);
